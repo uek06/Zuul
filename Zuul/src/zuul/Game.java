def36e28eb612eb085dcb5c5f6d72c05aa182a21
Ugo <ugo.elkoubi@wanadoo.fr>
2014-11-20 09:25:21 +0100

    mec

--- Zuul/src/zuul/Game.java
+++ Zuul/src/zuul/Game.java
@@ -5,13 +5,13 @@ public class Game {
     private void createRooms(){
         Room corridor1, corridor2, corridor3, corridor4, corridor5;
         Room classroom1, classroom2, lab1, lab2, examRoom, library,lunchroom;
-        corridor1=new Room("dans le hall");
-        corridor2=new Room("dans un couloir");
-        corridor3=new Room("dans un couloir");
-        corridor4=new Room("dans un couloir");
-        corridor5=new Room("dans un couloir");
-        classroom1=new Room("dans l'amphi 1");
-        classroom2=new Room("dans l'amphi 2");
+        corridor1=new Room(Language.DESCRIPTIONCOULOIR);
+        corridor2=new Room(Language.DESCRIPTIONCOULOIR);
+        corridor3=new Room(Language.DESCRIPTIONCOULOIR);
+        corridor4=new Room(Language.DESCRIPTIONCOULOIR);
+        corridor5=new Room(Language.DESCRIPTIONCOULOIR);
+        classroom1=new Room(Language.DESCRIPTIONAMPHI+" 1");
+        classroom2=new Room(Language.DESCRIPTIONAMPHI+" 2");
         lab1=new Room("dans la salle de TD 1");
         lab2=new Room("dans la salle de TD 2");
         examRoom=new Room("dans la salle d'examen");

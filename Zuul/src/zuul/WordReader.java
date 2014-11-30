0ee1d5e99c109c5e1a060d2368666ef5d3b6eea6
Christophe <chriscappe@hotmail.fr>
2014-11-30 22:57:46 +0100

    de la merde

--- Zuul/src/zuul/WordReader.java
+++ Zuul/src/zuul/WordReader.java
@@ -7,6 +7,11 @@ public class WordReader {
 
 	private static Scanner sc = new Scanner(System.in);
 
+	
+	/**
+	 * Lit une chaine de caractères depuis le clavier
+	 * @return la chaine lue
+	 */
 	public static String getWord() {
 		String word = sc.nextLine();
 
@@ -15,12 +20,20 @@ public class WordReader {
 		return word;
 	}
 
+	/**
+	 * Lit un boolean depuis le clavier
+	 * @return le boolean lu
+	 * @throws InputMismatchException si l'utilisateur ne tape pas true ou false
+	 */
 	public static boolean getBoolean() throws InputMismatchException{
 			sc = new Scanner(System.in);
 			return sc.nextBoolean();
 	}
 
-	
+	/**
+	 * Lit un entier depuis le clavier
+	 * @return l'entier lu
+	 */
 	public static int getInt() {
 		try {
 			sc = new Scanner(System.in);
